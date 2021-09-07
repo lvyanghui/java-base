3470ae04ed4607dfb2c2d9003afd8b1045c35008
lvyanghui <550352082@qq.com>
2021-09-07 20:51:46 +0800

    feat: custom get fund

--- fund/src/main/java/com/lyh/App.java
+++ fund/src/main/java/com/lyh/App.java
@@ -1,11 +1,46 @@
 package com.lyh;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException{
+        List<String> total = getList("G:\\workspace\\基金\\total.txt");
+        List<String> one = getList("G:\\workspace\\基金\\one.txt");
+        List<String> three = getList("G:\\workspace\\基金\\three.txt");
+
+        total.retainAll(three);
+        total.forEach(item->{
+            System.out.println(item);
+        });
+        System.out.println("--------------");
+
+        three.retainAll(one);
+        three.forEach(item->{
+            System.out.println(item);
+        });
+    }
+
+    public static List<String> getList(String path)throws IOException {
+        File file = new File(path);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fileReader);
+        List<String> list = new ArrayList<>();
+
+        String line = reader.readLine();
+        while (line != null){
+            //System.out.println(line);
+            list.add(new String(line.getBytes(),"utf-8"));
+            line = reader.readLine();
+        }
+        return list;
     }
 }
